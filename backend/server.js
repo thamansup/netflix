a8b7040b85f29ff17d856bca7a7bfdc783c5fd3e
suman <thamansupa@gmail.com>
2025-03-15 16:00:17 +1100

    second commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectMongoDB from "./db/connectMongoDB.js";
+dotenv.config();
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT||8800 ;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+//app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
+
+//all routes goes here
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
+});
